2ecb521e1ab8edeac96755835470edaa158f3594
rumqus <commonthings@yandex.ru>
2022-08-25 20:55:03 +0300

    formating and commenting

--- Assets/Scripts/Locations.cs
+++ Assets/Scripts/Locations.cs
@@ -1,27 +1,21 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Locations : MonoBehaviour
 {
     public GameObject[] levelLocations; // массив с локациями
-
     public static List<GameObject> runtimeLocations = new List<GameObject>();
-
-    private void Update()
-    {
-        Debug.Log(runtimeLocations.Count);
-    }
-
+    
+    /// <summary>
+    /// метод удаления пройденной локации в рантайме
+    /// </summary>
     public static void DelRuntimeLocation() 
     {
         if (runtimeLocations.Count >=2 )
         {
-            Debug.Log(@$"объект удален {runtimeLocations[runtimeLocations.Count - 2].name}");
             Destroy(runtimeLocations[runtimeLocations.Count - 2]);
             
         }
     
-    }
-   
+    }   
 }
